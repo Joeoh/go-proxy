081aaae4aa670f4993c17787449545568910780f
Joe O'Hara <joe.is.ohara@gmail.com>
2017-02-17 08:34:24 +0000

    Domain blacklisting

--- proxy.go
+++ proxy.go
@@ -9,19 +9,88 @@ import (
 	"strconv"
 	"bytes"
 	"fmt"
+	"os"
+	"sync"
 )
 
 const _DEFAULTLOG = "/var/log/go-proxy.log"
+const _IP_BLACKLIST = "ip_blacklist.txt"
+const _DOMAIN_BLACKLIST = "domain_blacklist.txt"
+const _403_FILE = "403.html"
 
-var connections (chan string)
+var connections = make(chan string)
+
+var blacklist = struct {
+	sync.RWMutex
+	m map[string]bool
+}{m: make(map[string]bool)}
 
 func configureLogging() {
 	log.SetLevel(log.INFO)
-	//log.SetLevel(log.DEBUG)
+	log.SetLevel(log.DEBUG)
 
 	if err := log.OpenFile(_DEFAULTLOG, log.FLOG_APPEND, 0644); err != nil {
 		log.Fatalf("Unable to open log file : %s", err)
 	}
+}
+
+/*
+func addToBlacklistFile(item string, file os.File) {
+	fileWriter := bufio.NewWriter(file)
+	fileWriter.WriteString(item+"\n")
+	fileWriter.Flush()
+}
+*/
+
+func addToBlacklist(item string) {
+	blacklist.Lock()
+	blacklist.m[item] = true
+	blacklist.Unlock()
+}
+
+func checkBlacklist(item string) bool {
+	defer blacklist.RUnlock()
+	blacklist.RLock()
+	res := blacklist.m[item]
+	log.Info(res)
+	return blacklist.m[item]
+}
+
+func isBlocked(domain string) bool {
+	if checkBlacklist(domain) {
+		return true
+	}
+	//TODO: Add code here to allow for lookup for *.blank etc
+	return false
+}
+
+func configureDomainBlacklist() {
+	domainBlacklistFile, err := os.Open(_DOMAIN_BLACKLIST)
+	bufferedReader := bufio.NewReader(domainBlacklistFile)
+
+	if err != nil {
+		log.Info("No domain blacklist or couldn't read file")
+		return
+	}
+	for {
+		line, _, err := bufferedReader.ReadLine()
+		lineString := string(line)
+		if err != nil {
+			break
+		}
+		if strings.Contains(lineString, "#") {
+			nonComment := strings.Split(lineString, "#")[0]
+			addToBlacklist(nonComment)
+		} else {
+			addToBlacklist(lineString)
+		}
+	}
+
+}
+
+func configureBlackLists() {
+
+	configureDomainBlacklist()
 }
 
 // Send required response to HTTPS Tunnelling request as per spec https://tools.ietf.org/html/draft-luotonen-ssl-tunneling-03
@@ -63,12 +132,11 @@ func getHostAndPortFromHostHeader(hostHeader string) (string, int, error) {
 }
 
 func getResourceFromHeader(header string, host string, port int) (string) {
-	portString := ":"+strconv.Itoa(port)
-	r := strings.NewReplacer("https://"+host, "", "http://"+host, "",portString, "")
+	portString := ":" + strconv.Itoa(port)
+	r := strings.NewReplacer("https://"+host, "", "http://"+host, "", portString, "")
 	resource := r.Replace(header)
 	return resource
 }
-
 
 func connectToHost(host string, port int) (*net.TCPConn, error) {
 
@@ -89,8 +157,11 @@ func connectToHost(host string, port int) (*net.TCPConn, error) {
 }
 
 //Copy the contents of a buffer from source to dst
-func copy(srcReadBuffer *bufio.Reader, dstWriteBuffer *bufio.Writer, dstConn *net.TCPConn) {
-	defer dstConn.Close()
+func copy(srcReadBuffer *bufio.Reader, dstWriteBuffer *bufio.Writer, dstConn *net.TCPConn, srcConn *net.TCPConn) {
+	//defer dstConn.Close()
+	//defer srcConn.Close()
+	defer closeWithLog(srcConn)
+	defer closeWithLog(dstConn)
 	for {
 		readByte, err := srcReadBuffer.ReadByte()
 		if err != nil {
@@ -118,6 +189,11 @@ func checkIsHttps(clientReadBuffer *bufio.Reader) (isHttps bool, err error) {
 	return
 }
 
+func closeWithLog(conn *net.TCPConn) {
+	log.Debugf("Closing conn from: %v to %v", conn.LocalAddr(), conn.RemoteAddr())
+	conn.Close()
+}
+
 func proxyData(clientConn *net.TCPConn) {
 	log.Debugf("Beginning to proxy")
 
@@ -129,7 +205,8 @@ func proxyData(clientConn *net.TCPConn) {
 	isHttps, err := checkIsHttps(clientReadBuffer)
 	if err != nil {
 		log.Debugf("Error peaking client connection buffer", err)
-		clientConn.Close()
+		closeWithLog(clientConn)
+		//clientConn.Close()
 	}
 	port := 80
 	host := ""
@@ -145,7 +222,8 @@ func proxyData(clientConn *net.TCPConn) {
 
 		if err != nil {
 			log.Debugf("Error reading from client connection buffer", err)
-			clientConn.Close()
+			closeWithLog(clientConn)
+			//clientConn.Close()
 			break
 		}
 
@@ -157,68 +235,86 @@ func proxyData(clientConn *net.TCPConn) {
 			break
 		} else {
 			//Write headers to new buffer to forward for http
-			clientHeaderBuffer.WriteString(messageString)
+			//Exclude Proxy-Connection Header
+			if !strings.Contains(messageString, "Proxy-Connection: Keep-Alive") {
+				clientHeaderBuffer.WriteString(messageString)
+
+			}
 		}
 		if strings.HasPrefix(messageString, "Host:") {
 			//Extract host and port
 			host, port, err = getHostAndPortFromHostHeader(messageString)
 
 			if err != nil {
-				clientConn.Close()
+				closeWithLog(clientConn)
+				//clientConn.Close()
 				break
 			}
 
 			log.Debugf("Extracting host from:  %v =@@= %v", string(message), host)
 			seenHostLine = true
-		} else if strings.HasPrefix(messageString, "GET ") {
+		} else if strings.HasPrefix(messageString, "GET ") || strings.HasPrefix(messageString, "POST ") {
 			resourceLine = messageString
 			seenResourceLine = true
 		}
 	}
 	if !seenHostLine || (!seenResourceLine && !isHttps) {
-		clientConn.Close()
+		//clientConn.Close()
+		closeWithLog(clientConn)
 		return
 	}
 
+	if isBlocked(host) {
+		sendBlockedMessage(clientWriteBuffer, host)
+		closeWithLog(clientConn)
+		return
+	}
 
 	resourceHeader := getResourceFromHeader(resourceLine, host, port)
 
 	remoteConn, err := connectToHost(host, port)
 	if err != nil {
 		log.Debugf("Failed connecting to: %v on behalf of: %v \n", net.JoinHostPort(host, strconv.Itoa(port)), clientConn.RemoteAddr().String())
-		clientConn.Close()
+		closeWithLog(clientConn)
+		//clientConn.Close()
 		return
 	}
 
 	remoteWriteBuffer := bufio.NewWriter(remoteConn)
 	remoteReadBuffer := bufio.NewReader(remoteConn)
 
-	connections <- fmt.Sprintf("Connected to: %v on behalf of: %v \n", remoteConn.RemoteAddr().String(), clientConn.RemoteAddr().String());
+	go func() {
+		connections <- fmt.Sprintf("Connected to: %v on behalf of: %v \n", remoteConn.RemoteAddr().String(), clientConn.RemoteAddr().String())
+	}();
 	log.Debugf("Connected to: %v on behalf of: %v \n", remoteConn.RemoteAddr().String(), clientConn.RemoteAddr().String())
 
 	if isHttps {
 		err = sendSSLTunnellingResponse(clientWriteBuffer)
 		if err != nil {
-			clientConn.Close()
-			remoteConn.Close()
+			closeWithLog(clientConn)
+			//clientConn.Close()
+			closeWithLog(remoteConn)
+			//remoteConn.Close()
 			return
 		}
 	} else {
 		err = sendClientHeadersToHost(clientHeaderBuffer, remoteWriteBuffer, resourceHeader)
 		if err != nil {
-			clientConn.Close()
-			remoteConn.Close()
+			closeWithLog(clientConn)
+			//clientConn.Close()
+			closeWithLog(remoteConn)
+			//remoteConn.Close()
 			return
 		}
 	}
 	//Copy data from Server into client and client to server concurrently
 
-	//Server into client
-	go copy(remoteReadBuffer, clientWriteBuffer, clientConn)
-	//Client into server
-	go copy(clientReadBuffer, remoteWriteBuffer, remoteConn)
-}
 
+	//Server into client
+	go copy(remoteReadBuffer, clientWriteBuffer, clientConn, remoteConn)
+	//Client into server
+	go copy(clientReadBuffer, remoteWriteBuffer, remoteConn, clientConn)
+}
 
 func sendClientHeadersToHost(headers bytes.Buffer, hostBuffer *bufio.Writer, resourceHeader string) (err error) {
 
@@ -246,21 +342,48 @@ func sendClientHeadersToHost(headers bytes.Buffer, hostBuffer *bufio.Writer, res
 	return
 }
 
+func write403Contents(clientWriteBuffer *bufio.Writer, host string) {
+	defer clientWriteBuffer.Flush()
+	domainBlacklistFile, err := os.Open(_403_FILE)
+	bufferedReader := bufio.NewReader(domainBlacklistFile)
+	//No file to read, write simple
+	if err != nil {
+		forbiddenString := "Your system admin has blocked access to " + host
+		clientWriteBuffer.WriteString(forbiddenString)
+		return
+	}
+	r := strings.NewReplacer("%HOST%", host)
 
+	for {
+		line, _, err := bufferedReader.ReadLine()
+		lineString := string(line)
+		if err != nil {
+			break
+		}
+		//Replace all occurrences of host placeholder with actual host
+		lineString = r.Replace(lineString)
+		clientWriteBuffer.WriteString(lineString)
+	}
+}
 
-func connectionPrinter(){
+func sendBlockedMessage(clientWriteBuffer *bufio.Writer, host string) {
+	clientWriteBuffer.WriteString("HTTP/1.1 403 FORBIDDEN\r\n\r\n")
+	write403Contents(clientWriteBuffer, host)
+	clientWriteBuffer.Flush()
+
+}
+
+func connectionPrinter() {
 	for {
 		connection := <-connections
 		log.Infof("Connection info: %v", connection)
 	}
 }
 
-
 func main() {
 	configureLogging()
-
-	connections = make(chan string)
-	go connectionPrinter()
+	configureBlackLists()
+	//go connectionPrinter()
 
 	lnaddr, err := net.ResolveTCPAddr("tcp", ":8080")
 	if err != nil {
@@ -281,7 +404,7 @@ func main() {
 			log.Infof("Error accepting connection: %v\n", err)
 			continue
 		}
-		log.Debugf("New Conn\n")
+		log.Debugf("New Conn from %v", conn.RemoteAddr())
 		go proxyData(conn)
 	}
 }
